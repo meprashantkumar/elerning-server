fa2fa29915c19ab98cd771170d8fecb7edfbd062
Prashant kumar Chaturvedi <93850004+meprashantkumar@users.noreply.github.com>
2024-11-10 09:29:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ import dotenv from "dotenv";
 import { connectDb } from "./database/db.js";
 import Razorpay from "razorpay";
 import cors from "cors";
+import axios from "axios";
+
+const url = `https://elerning-server.onrender.com`;
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+dotenv.config();
 
 dotenv.config();
 
